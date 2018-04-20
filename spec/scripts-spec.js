ca5f8111d2de4e7e8ef45b1084003a950532f007
julia sheremet <juliajessicasher@gmail.com>
2018-04-20 16:29:26 -0700

    fix syntax in spec and js files

--- spec/scripts-spec.js
+++ spec/scripts-spec.js
@@ -13,6 +13,13 @@ describe('galacticAge', function() {
     expect(output).toEqual(788400000);
   });
 
+  it('should take two dates and determine the difference in seconds, between the two', function() {
+    let testAge = new galacticAge(06/07/1993, 06/07/2030);
+    let output = testAge.getSecondsDate(testAge.date1, testAge.date2);
+    // let output = parseInt(convert); do this in the UI
+    expect(output).toEqual(7.838836349461016e-9);
+  });
+
   it('should return the age of a human in Mercury years.', function() {
     let testAge = new galacticAge(25);
     let output = testAge.getAgeMercury(testAge.age);
@@ -21,21 +28,27 @@ describe('galacticAge', function() {
   });
 
   it('should return the age of a human in Venus years', function() {
-    let testAge = new galacticAge(24);
+    let testAge = new galacticAge(25);
     let output = testAge.getAgeVenus(testAge.age);
-    expect(output).toEqual(38.93333333333333);
+    expect(output).toEqual(40.55555555555556);
   });
 
   it('should return the age of a human in Mars years', function() {
-    let testAge = new galacticAge(24);
-    let output = testAge.getAgeVenus(testAge.age);
-    expect(output).toEqual(38.93333333333333);
+    let testAge = new galacticAge(25);
+    let output = testAge.getAgeMars(testAge.age);
+    expect(output).toEqual(13.282387190684133);
   });
 
-  it('should return the age of a human in Mars years', function() {
-    let testAge = new galacticAge(24);
+  it('should return the age of a human in Jupiter years', function() {
+    let testAge = new galacticAge(25);
     let output = testAge.getAgeJupiter(testAge.age);
-    expect(output).toEqual(742.3728813559321);
+    expect(output).toEqual(773.3050847457627);
+  });
+
+  it('should calculate life expectancy on Earth', function() {
+    let testAge = new galacticAge(25);
+    let output = testAge.expectancyAge(testAge.age);
+    expect(output).toEqual(62);
   });
   // it('should will take two dates and determine the difference in seconds between the two.', function() {
   //   let testDate1 = new galacticAge(1993, 06, 07); //first date
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -1,11 +1,27 @@
 class galacticAge {
-  constructor(age) {
+  constructor(age, expectancy, date1, date2) {
     this.age = age;
+    this.expectancy = 87;
+    this.date1 = new Date(date1);
+    this.date2 = new Date(date2);
     // this.planet = ["mercury", "venus", "mars"];
     // this.ageToDays = age * 365;
   }
 
-  getSeconds(userAge) {
+  getSecondsDate() {
+    // let date1 = str1.split('/');
+    let startDate = this.date1;
+    // let startDateNum = Date.parse(startDate);
+    let endDate = this.date2;
+    // let endDateNum = Date.parse(endDate);
+    let seconds = Math.abs((startDate.getTime() - endDate.getTime()) / 1000);
+    // let seconds = (startDate - endDate) / 1000;
+    return seconds;
+  }
+
+
+
+  getSeconds(userAge) { //seconds in a year
     let ageToSeconds = userAge;
     let calculatedAge = ageToSeconds;
     return calculatedAge * 365 * 24 * 60 * 60;
@@ -35,6 +51,19 @@ class galacticAge {
     return jupiterAge;
   }
 
+  expectancyAge(userAge){
+    const age = userAge;
+    let leftToLive = this.expectancy - age;
+    return leftToLive;
+  }
+
+  // expectancyEarth(userAge){
+  //   const age = userAge;
+  //   let leftToLive = age - this.expectancy;
+  //   if (leftToLive > 87){
+  //     return LeftToLive;
+  //   }
+  // }
 
   // getSecondsDate(userDate1, userDate2) {
   //   let testDate1 = userDate1;
