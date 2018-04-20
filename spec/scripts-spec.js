ec938721bae65238ad2e6d23b2139eca43887f9e
julia sheremet <juliajessicasher@gmail.com>
2018-04-20 08:24:52 -0700

    run initial npm and karma test to validate plugins are working

--- spec/scripts-spec.js
+++ spec/scripts-spec.js
@@ -1,8 +1,8 @@
-import { Name } from './../src/scripts.js';
-
-describe('name', function() {
-
-  it('should test whether a number matches another number', function() {
-    expect(1).toEqual(1);
-  });
-});
+// import { Name } from './../src/scripts.js';
+//
+// describe('name', function() {
+//
+//   it('should test whether a number matches another number', function() {
+//     expect(1).toEqual(1);
+//   });
+// });
